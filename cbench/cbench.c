d24afcc9ecbc5c8ce1c0e961edd1bf9caec9de07
Manuel Wesenjak <manuel.wesenjak@student.uibk.ac.at>
2016-11-29 12:56:43 +0100

    yolo

---
+++ cbench/cbench.c
@@ -0,0 +1,9 @@
+//
+//  cbench.c
+//  cbench
+//
+//  Created by Profile 1 on 29.11.16.
+//  Copyright © 2016 Profile 1. All rights reserved.
+//
+
+#include "cbench.h"
---
+++ cbench/cbench.h
@@ -0,0 +1,14 @@
+//
+//  cbench.h
+//  cbench
+//
+//  Created by Profile 1 on 29.11.16.
+//  Copyright © 2016 Profile 1. All rights reserved.
+//
+
+#ifndef cbench_h
+#define cbench_h
+
+#include <stdio.h>
+
+#endif /* cbench_h */
